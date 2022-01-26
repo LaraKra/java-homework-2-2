a6d256e2ffaa54d241763fa864c9070090e49ae9
LaraKra <konlor@rambler.ru>
2022-01-26 11:25:32 +0300

    New commit two

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-        int startAccount = 100;
-        int amountUp = 1200;
-        int bonus = amountUp / 100;
-        int totalSum = startAccount + amountUp;
-        boolean startBonus = amountUp > 1000;
-
-        if (startBonus) {
-            int finalBill = totalSum + bonus;
-            System.out.println("Итоговая сумма на счету клиента - " + (finalBill) + " руб.");
-            System.out.println("Бонус равен - " + bonus + " руб.");
-        } else {
-            int finalBill = totalSum;
-            System.out.println("Итоговая сумма на счету клиента - " + (finalBill) + " руб.");
+        float startAccount = 100;
+        float amountUp = 1200;
+        int bonus;
+        if (amountUp > 1000) {
+            System.out.println("Бонус равен - " + (bonus = (int) amountUp / 100) + " руб.");
+            System.out.println("Итоговая сумма на счету клиента - " + (startAccount = bonus + amountUp + startAccount) + " руб.");
+        }
+        if (amountUp < 1000) {
+            System.out.println(bonus = 0);
+            System.out.println("Итоговая сумма на счету клиента - " + (startAccount = startAccount + amountUp) + " руб.");
         }
 
     }
